9ed98e4a77bfdcc0027134adfc6b9080eedd5e6b
Alberto Di Martino <albertodmartino@gmail.com>
2016-10-30 00:18:29 -0700

    Add Favorite Movies DB contract

---
+++ app/src/main/java/albertodimartino/com/popularmovies/data/MovieContract.java
@@ -0,0 +1,54 @@
+package albertodimartino.com.popularmovies.data;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+import android.text.format.Time;
+
+public class MovieContract {
+
+    public static final String CONTENT_AUTHORITY = "albertodimartino.com.popularmovies.data";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_MOVIE = "movie";
+
+    // Movie Table
+    public static final class MovieEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIE).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_MOVIE;
+
+        // Table name
+        public static final String TABLE_NAME = "favorite_movie";
+
+        // Columns
+        public static final String MOVIE_PROVIDER_ID = "movie_provider_id";
+        public static final String MOVIE_TITLE = "title";
+        public static final String RELEASE_DATE = "release_date";
+        public static final String OVERVIEW = "overview";
+        public static final String POSTER_PATH = "poster_path";
+        public static final String VOTE_AVERAGE = "vote_average";
+
+        public static Uri buildMovieUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+
+    // Helpers
+
+    // To make it easy to query for the exact date, we normalize all dates that go into
+    // the database to the start of the the Julian day at UTC.
+    public static long normalizeDate(long startDate) {
+        // normalize the start date to the beginning of the (UTC) day
+        Time time = new Time();
+        time.set(startDate);
+        int julianDay = Time.getJulianDay(startDate, time.gmtoff);
+        return time.setJulianDay(julianDay);
+    }
+}
---
+++ app/src/main/java/albertodimartino/com/popularmovies/data/MovieDbHelper.java
@@ -0,0 +1,39 @@
+package albertodimartino.com.popularmovies.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import albertodimartino.com.popularmovies.data.MovieContract.MovieEntry;
+
+public class MovieDbHelper extends SQLiteOpenHelper {
+
+    // If you change the database schema, you must increment the database version.
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "movie.db";
+
+    public MovieDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // Create a table to hold movies.
+        final String SQL_CREATE_FAVORITE_MOVIE_TABLE = "CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+                MovieEntry._ID + " INTEGER PRIMARY KEY," +
+                MovieEntry.MOVIE_PROVIDER_ID+ " TEXT UNIQUE NOT NULL, " +
+                MovieEntry.MOVIE_TITLE + " TEXT NOT NULL, " +
+                MovieEntry.RELEASE_DATE + " INTEGER NOT NULL, " +
+                MovieEntry.OVERVIEW + " TEXT, " +
+                MovieEntry.POSTER_PATH + " TEXT, " +
+                MovieEntry.VOTE_AVERAGE + " REAL " +
+                " );";
+        sqLiteDatabase.execSQL(SQL_CREATE_FAVORITE_MOVIE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXIST " + MovieEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
